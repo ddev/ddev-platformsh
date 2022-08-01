e9ad58a270317266bfc74d8a3335659be3ffcbf1
Randy Fay <randy@randyfay.com>
2022-07-31 19:20:30 -0600

    Download all mounts, fixes #19, fixes #20, fixes #2 (#21)
    * Require config-star-yaml-merging of ddev
    * Support post_deploy hooks, fixes #20
    * Download all mounts, use direct platform.yaml
    * Use files_import_command, not files_pull_command
    * Move environment variables into config.platformsh.yaml
    * Use platform mount:download --all
    * Turn off all github tests for now

--- homeadditions/.bashrc.d/platformsh-environment.sh
+++ homeadditions/.bashrc.d/platformsh-environment.sh
@@ -1,32 +1,4 @@
 #ddev-generated
-export PATH=$PATH:/var/www/html/.global/bin
-# TODO: Review which of these matters and which can be dummied up
-# Consider commit hash for PLATFORM_TREE_ID
-export PLATFORM_TREE_ID=2dc356f2fea13ef683f9adc5fc5bd28e05ad992a
-export PLATFORM_PROJECT_ENTROPY="$(echo $RANDOM | shasum -a 256 | awk '{print $1}')"
-export PLATFORM_APP_DIR=/var/www/html
-export PLATFORM_DIR=${PLATFORM_APP_DIR}
-export PLATFORM_BRANCH=${PLATFORM_ENVIRONMENT}
-
-platform_routes=$(cat <<-ENDROUTES
-{
-  "${DDEV_PRIMARY_URL}": {
-    "primary": true,
-    "id": null,
-    "production_url": "${DDEV_PRIMARY_URL}",
-    "attributes": {},
-    "upstream": "drupal",
-    "type": "upstream",
-    "original_url": "https://{default}/"  }
-}
-ENDROUTES
-)
-
-export PLATFORM_ROUTES="$( if base64 --version >/dev/null 2>&1; then echo -n ${platform_routes} | base64 -w0; else echo -n ${platform_routes} | base64; fi)"
-# TODO: Use real PLATFORM_VARIABLES?
-PLATFORM_VARIABLES=e30=
-# TODO: Consider populating $USER in web container by default
-USER=$(id -u -n)
 
 for item in .global/environment .environment; do
   if [ -f "${item}" ]; then

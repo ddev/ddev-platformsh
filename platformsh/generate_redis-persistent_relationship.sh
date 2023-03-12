be5c83d4f7bd6ddfa26eca5afd389d2baf902b7a
Randy Fay <randy@randyfay.com>
2023-03-11 19:07:28 -0700

    Use a regular file - symlink fails

--- platformsh/generate_redis-persistent_relationship.sh
+++ platformsh/generate_redis-persistent_relationship.sh
@@ -1,1 +1,30 @@
-generate_redis_relationship.sh
+#!/usr/bin/env bash
+
+#ddev-generated
+
+export relationshipname=$1
+
+read -r -d '' redis_stanza <<REDIS_EOF
+  "${relationshipname}": [
+    {
+      "username": null,
+      "scheme": "redis",
+      "service": "cache",
+      "fragment": null,
+    "ip": "255.255.255.255",
+      "hostname": "redis",
+      "public": false,
+      "cluster": "ddev-dummy-cluster",
+      "host": "redis",
+      "rel": "redis",
+      "query": {},
+      "path": null,
+      "password": null,
+      "type": "redis:6.0",
+      "port": 6379,
+      "host_mapped": false
+    }
+  ]
+REDIS_EOF
+
+printf "$redis_stanza"

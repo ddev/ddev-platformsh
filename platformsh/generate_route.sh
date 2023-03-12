d225fd4c2bf2b7696ceb63860fbdc980545f23c8
Randy Fay <randy@randyfay.com>
2023-03-11 18:04:34 -0700

    Another pass maybe closer

---
+++ platformsh/generate_route.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#ddev-generated
+
+# Create a stanza for $PLATFORM_ROUTES
+
+# Args:
+# route (like https://{default}/)
+# id
+# production_url
+# upstream
+# type
+# original_url
+
+export route=$1
+export id=$2
+export production_url=$3
+export upstream=$4
+export type=$5
+export original_url=$6
+
+
+read -r -d '' route_stanza <<ROUTE_EOF
+"${route}": {
+    "primary": true,
+    "id": "${id}",
+    "production_url": "${production_url}",
+    "attributes": {},
+    "upstream": "${upstream}",
+    "type": "${type}",
+    "original_url": "${original_url}"
+  }
+ROUTE_EOF
+
+printf "${route_stanza}"

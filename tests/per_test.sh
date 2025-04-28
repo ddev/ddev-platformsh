361057693c233c91d7d7e9aaa806ff0d1907d49f
Randy Fay <randy@randyfay.com>
2025-04-28 09:53:23 -0600

    test: Fix up some remnants of old usage (#137)

--- tests/per_test.sh
+++ tests/per_test.sh
@@ -5,12 +5,15 @@ per_test_setup() {
   set +u
   ddev delete -Oy ${PROJNAME} >/dev/null 2>&1 || true
   rm -rf ${TESTDIR} && mkdir -p ${TESTDIR} && cd ${TESTDIR}
-  curl -sfL -o /tmp/testtemplate.tgz "https://github.com/platformsh-templates/${template}/tarball/master"
-  tar -zxf /tmp/testtemplate.tgz --strip-components=1
+  run curl -sfL -o /tmp/testtemplate.tgz "https://github.com/platformsh-templates/${template}/tarball/master"
+  assert_success
+  run tar -zxf /tmp/testtemplate.tgz --strip-components=1
+  assert_success
   # If the template happens to have a .ddev directory, remove as it can break things
   rm -rf .ddev
   # Start with bogus settings so we know we got the right stuff when testing
-  ddev config --project-name=${PROJNAME} --php-version=5.6 --database=mariadb:10.1 --docroot=x --project-type=php --web-environment-add=PLATFORMSH_CLI_TOKEN=notokenrightnow,PLATFORM_PROJECT=notyet,PLATFORM_ENVIRONMENT=notyet
+  run ddev config --project-name=${PROJNAME} --php-version=5.6 --database=mariadb:10.1 --docroot=x --project-type=php --web-environment-add=PLATFORMSH_CLI_TOKEN=notokenrightnow,PLATFORM_PROJECT=notyet,PLATFORM_ENVIRONMENT=notyet
+  assert_success
   echo "# doing ddev add-on get $source with template ${template} PROJNAME=${PROJNAME} in ${TESTDIR} ($(pwd))" >&3
   run ddev add-on get ${PROJECT_SOURCE}
   assert_success
